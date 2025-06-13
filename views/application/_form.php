2513af36dd44411cbb391bee5450ec038ad190ee
cacko <alex@cacko.net>
2025-06-13 14:38:14 +0100

    add controllers, view, migrations

---
+++ views/application/_form.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Application $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="application-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'date_of_birth')->widget(\yii\jui\DatePicker::class, [
+        'dateFormat' => 'yyyy-MM-dd',
+        'options' => ['class' => 'form-control'],
+    ]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'income')->textInput() ?>
+
+    <?= $form->field($model, 'number_of_dependants')->textInput() ?>
+
+    <!--    --><?php //= $form->field($model, 'created_at')->textInput() ?>
+    <!---->
+    <!--    --><?php //= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
